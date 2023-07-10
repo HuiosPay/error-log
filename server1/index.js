fc6d964dfe53bd9cf1b90cd0d97198de424d0bdb
kumputa <kumputaisaac@gmail.com>
2023-07-10 16:52:04 +0100

    add

--- server1/index.js
+++ server1/index.js
@@ -1,18 +1,29 @@
-const express = require("express");
-const cors = require("cors");
-
 const server = require("http").createServer();
-const io = require("socket.io")(server);
-
-const app = express();
-
-app.use(cors());
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
 
 io.on("connection", (socket) => {
   console.log("s1 is connected to s2");
 
-  socket.emit("message", "hello from s11");
+  simulateDatabaseError(socket);
 });
+
+function simulateDatabaseError(socket) {
+  // create a database error for now
+  const error = "Database connection error server 1";
+
+  // Emit the error message to Server 2
+  socket.emit("message", error);
+
+  // Emit the error message to frontend
+  socket.emit("messagetofrontend", error);
+}
 
 server.listen(3000, () => {
   console.log("running on port 3000");
